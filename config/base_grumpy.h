a68baeedd7f314be940a665f3aea6a9192426658
kilipan <kilipan@tutanota.com>
2023-11-02 07:22:09 +0100

    add combo shifting

--- config/base_grumpy.h
+++ config/base_grumpy.h
@@ -46,7 +46,7 @@ combo_##NAME { \
         COMBO(sch, &macro_sch, 0 2)
         COMBO(slash, &kp SLASH, 23 24)
         COMBO(dash, &kp MINUS, 22 23)
-        COMBO(enter, &kp ENTER, 22 23 24)
+        COMBO(enter, &kp ENTER, 22 24)
         COMBO(esc, &kp ESC, 20 21)
 /* parentheticals */
         COMBO(lbrc, &kp LBRC, 0 9)
@@ -64,8 +64,44 @@ combo_##NAME { \
         COMBO(bspc, &kp BSPC, 5 6)
         COMBO(del, &kp DEL, 6 7)
         COMBO(delword, &kp LC(BSPC), 5 7)
+/* alternative shifting */
+        COMBO(Sq, &kp LS(Q), 1 2 26)
+        COMBO(Sz, &kp LS(Z), 18 19 26)
+        COMBO(Sv, &kp LS(V), 0 1 26)
+        COMBO(Ssch, &macro_shifted_sch, 0 2 26)
+        COMBO(Sw, &kp LS(W), 0 26)
+        COMBO(Sf, &kp LS(F), 1 26)
+        COMBO(Sp, &kp LS(P), 2 26)
+        COMBO(Sb, &kp LS(B), 3 26)
+        COMBO(Sj, &kp LS(J), 4 27)
+        COMBO(Sl, &kp LS(L), 5 27)
+        COMBO(Su, &kp LS(U), 6 27)
+        COMBO(Sy, &kp LS(Y), 7 27)
+        COMBO(Sr, &kp LS(R), 8 26)
+        COMBO(Ss, &kp LS(S), 9 26)
+        COMBO(St, &kp LS(T), 10 26)
+        COMBO(Sh, &kp LS(H), 11 26)
+        COMBO(Sk, &kp LS(K), 12 26)
+        COMBO(Sx, &kp LS(X), 13 27)
+        COMBO(Sn, &kp LS(N), 14 27)
+        COMBO(Sa, &kp LS(A), 15 27)
+        COMBO(Si, &kp LS(I), 16 27)
+        COMBO(So, &kp LS(O), 17 27)
+        COMBO(Sc, &kp LS(C), 19 26)
+        COMBO(Sg, &kp LS(G), 20 26)
+        COMBO(Sd, &kp LS(D), 21 26)
+        COMBO(Sm, &kp LS(M), 22 27)
+        COMBO(Scomma, &kp SEMI, 23 27)
+        COMBO(Sdot, &kp COLON, 24 27)
+        COMBO(Se, &kp LS(E), 14 15 27)
     };
 };
+//   ____________________________  ____________________________
+//  /       0  |  1  |  2  |  3  \/   4 |   5 |   6 |   7      \
+// |  8  |  9  | 10  | 11  | 12  /\  13 |  14 |  15 |  16 | 17  |
+// | 19  | 19  | 20  | 21       /  \       22 |  23 |  24 | 25  |
+//                      \  26  /    \  27  /
+//                       ------      ------
 
 
 / {
